b6014ce62b0d83fe597a9c1b09a8cfd29526a5f2
mayank dutta <smayankdutt@gmail.com>
2023-06-27 02:39:51 +0530

    template prepared

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-const express = require('express');
-
+const express = require("express");
+const { PORT } = require("./config/serverConfig");
+const bodyParser = require("body-parser");
 
 const setupAndStartServer = async () => {
   const app = express();
 
-}
+  app.use(bodyParser.json()); // middleware
+  app.use(bodyParser.urlencoded({ extended: true })); // middleware
+
+  app.listen(3000, () => {
+    console.log(`Server started @port=${PORT}`);
+  });
+};
+
+setupAndStartServer();
