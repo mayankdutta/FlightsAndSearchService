83c4839a6b772ab15bdc603fd83d9b4825c996ea
mayank dutta <smayankdutt@gmail.com>
2023-06-28 04:15:30 +0530

    added city service

---
+++ src/repository/index.js
@@ -0,0 +1,4 @@
+// to include all the import an one place so that avoid multiple require statement in other files.
+module.exports = {
+  CityRepositry: require("./city-repository"),
+};
---
+++ src/services/city-service.js
@@ -0,0 +1,44 @@
+const { CityRepositry } = require("../repository/index");
+
+class CityService {
+  constructor() {
+    this.cityRepository = new CityRepositry();
+  }
+
+  async createCity({ name }) {
+    try {
+      const response = await this.cityRepository.createCity({ name });
+      return response;
+    } catch (error) {
+      console.log("something went wrong in service layer");
+      return { error };
+    }
+  }
+  async deleteCity(cityId) {
+    try {
+      const response = await this.cityRepository.deleteCity(cityId);
+      return response;
+    } catch (error) {
+      console.log("something went wrong in service layer");
+      return { error };
+    }
+  }
+  async updateCity(cityId) {
+    try {
+      const response = await this.cityRepository.updateCity(cityId);
+      return response;
+    } catch (error) {
+      console.log("something went wrong in service layer");
+      return { error };
+    }
+  }
+  async getCity(cityId) {
+    try {
+      const response = await this.cityRepository.getCity(cityId);
+      return response;
+    } catch (error) {
+      console.log("something went wrong in service layer");
+      return { error };
+    }
+  }
+}
